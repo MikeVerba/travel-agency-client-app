52ce58130efe32cdaba978b376567c0d94329991
MikeVerba <michal.wierba@gmail.com>
2019-08-04 13:25:26 +0200

    new tests added 2

--- src/test/java/com/example/travelagencyclientapp/app/controllers/IndexControllerTest.java
+++ src/test/java/com/example/travelagencyclientapp/app/controllers/IndexControllerTest.java
@@ -1,9 +1,11 @@
 package com.example.travelagencyclientapp.app.controllers;
 
+import com.example.travelagencyclientapp.app.models.entities.UserEntity;
 import com.example.travelagencyclientapp.app.models.services.UserService;
 import com.example.travelagencyclientapp.app.models.services.UserSession;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,13 +13,15 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
 
 
-import static org.mockito.Mockito.reset;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class IndexControllerTest {
@@ -30,6 +34,9 @@ class IndexControllerTest {
 
     @InjectMocks
     IndexController indexController;
+
+    @Mock
+    Model model;
 
     MockMvc mockMvc;
 
@@ -46,14 +53,45 @@ class IndexControllerTest {
     }
 
     @Test
-    void index() throws Exception{
+    @DisplayName("Testing index get with Mock Mvc")
+    void indexTestingWithMockMvc() throws Exception{
 
 
-        MvcResult mvcResult = mockMvc.perform(get("/index/"))
+        given(userSession.isLogin()).willReturn(true);
+        given(userService.getLoggedUser()).willReturn(new UserEntity());
+
+        mockMvc.perform(get("/index/"))
                 .andExpect(status().isOk())
-                .andReturn();
+                .andExpect(model().hasNoErrors())
+                .andExpect(model().size(1))
+                .andExpect(model().attributeExists("userEntity"))
+                .andExpect(view().name("user-details"));
+
+    }
 
 
+    @Test
+    @DisplayName("Testing index with BDD")
+    void indexTestWithBDD(){
+
+        //given
+        given(userSession.isLogin()).willReturn(true);
+        given(userService.getLoggedUser()).willReturn(new UserEntity());
+
+        //when
+        String resultView = indexController.index(model);
+        //then
+
+        assertEquals(resultView,"user-details");
+
+        verify(userSession,times(1)).isLogin();
+        verify(userSession,never()).logout();
+        verify(userSession,never()).getUserEntity();
+
+        verify(userService,times(1)).getLoggedUser();
+        verify(userService,never()).findById(anyLong());
+        verify(userService,never()).addUser(anyObject());
+        verify(userService,never()).logoutUser();
     }
 
 }
--- src/test/java/com/example/travelagencyclientapp/app/controllers/RegistrationControllerTest.java
+++ src/test/java/com/example/travelagencyclientapp/app/controllers/RegistrationControllerTest.java
@@ -2,9 +2,7 @@ package com.example.travelagencyclientapp.app.controllers;
 
 import com.example.travelagencyclientapp.app.models.forms.RegistrationForm;
 import com.example.travelagencyclientapp.app.models.services.UserService;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -24,8 +22,8 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class RegistrationControllerTest {
@@ -71,14 +69,19 @@ class RegistrationControllerTest {
     }
 
     @Test
+    @DisplayName("Testing registration form get with Mock Mvc")
     void registrationFormGetTest() throws Exception {
 
         mockMvc.perform(get("/registration-form/"))
                 .andExpect(status().isOk())
+                .andExpect(model().attributeExists("registrationForm"))
+                .andExpect(model().size(1))
+                .andExpect(view().name("registration-form"))
                 .andExpect(model().attribute("registrationForm", instanceOf(RegistrationForm.class)));
     }
 
     @Test
+    @DisplayName("Testing registration form get with BDD")
     void registrationFormGetTestWithBDD() {
 
         //given
@@ -87,15 +90,42 @@ class RegistrationControllerTest {
 
         //when
 
-        String result = registrationController.registrationForm(registrationForm,bindingResult,model);
+        String resultView = registrationController.registrationForm(registrationForm,bindingResult,model);
 
 
         //then
 
         then(userService).should().addUser(registrationForm);
 
-        verify(bindingResult).hasErrors();
+        verify(bindingResult,times(1)).hasErrors();
+        verify(bindingResult,never()).addError(any());
+        verify(bindingResult,never()).getModel();
+        verify(userService,times(1)).addUser(any());
+        verify(userService,never()).findById(anyLong());
+        verify(userService,never()).logoutUser();
+        verify(userService,never()).updateUser(any());
+        verify(userService,never()).login(any());
+
+        assertEquals(resultView,"registration-success");
+
+    }
 
 
+    @Test
+    @Disabled
+    @DisplayName("Testing registration form post with Mock Mvc")
+    void registrationFormPostTestWithMockMvc() throws Exception{
+
+        given(bindingResult.hasErrors()).willReturn(false);
+        given(userService.addUser(registrationForm)).willReturn(true);
+
+
+        mockMvc.perform(post("/registration-form"))
+                .andExpect(model().hasNoErrors())
+                .andExpect(view().name("registration-success"))
+                .andExpect(model().attributeExists("registrationForm"))
+                .andExpect(status().isOk());
+
+        //todo Circular view path Exception to fix
     }
 }
